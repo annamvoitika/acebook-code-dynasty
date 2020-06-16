8e47a9c624ede7c989079c17d6b930bda7bda10c
Anna <anna.voitik@gmail.com>
2020-06-16 15:34:09 +0100

    User authentication

--- app.js
+++ app.js
@@ -9,6 +9,72 @@ const postsRouter = require('./routes/posts');
 
 const app = express();
 
+const userInViews = require('./lib/middleware/userInViews');
+const authRouter = require('./routes/auth');
+const indexRouter = require('./routes/home');
+const usersRouter = require('./routes/users');
+
+const session = require('express-session');
+// config express-session
+const sess = {
+  secret: 'thisisasecret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+if (app.get('env') === 'production') {
+  // Use secure cookies in production (requires SSL/TLS)
+  sess.cookie.secure = true;
+
+  // Uncomment the line below if your application is behind a proxy (like on Heroku)
+  app.set('trust proxy', 1);
+}
+
+app.use(session(sess));
+
+// Load environment variables from .env
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Load Passport
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+
+// Configure Passport to use Auth0
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3030/posts'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(userInViews());
+app.use('/', authRouter);
+app.use('/', indexRouter);
+app.use('/', usersRouter);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const HomeController = require('../controllers/home');
+// const HomeController = require('../controllers/home');
 
-router.get('/', HomeController.Index);
+// router.get('/', HomeController.Index);
+router.get('/', function (req, res, next) {
+  res.render('home/index', { title: 'Auth0 Webapp sample Nodejs' });
+});
 
 module.exports = router;
