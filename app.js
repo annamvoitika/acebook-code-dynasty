93cd253d08c88ee08391f08364a3e363facec903
kate smith <kateesmith4994@gmail.com>
2020-06-18 12:10:18 +0100

    has a working form that you can submit a file to, doesn't go anywhere but in theory returns json data about the file

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// photo
+const multer = require('multer');
+const crypto = require('crypto');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const homeRouter = require('./routes/home');
 const postsRouter = require('./routes/posts');
 
@@ -18,6 +27,45 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// photo
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
+const mongoURI = 'mongodb+srv://acebook-code-dynasty:code-dynasty@acebook-code-dynasty-wqegs.mongodb.net/acebook-code-dynasty?retryWrites=true&w=majority';
+
+const conn = mongoose.createConnection(mongoURI);
+
+let gfs;
+
+conn.once('open', () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+})
+
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({storage});
+
+app.post('/upload', upload.single('file'),(req, res) => {
+  res.json({file: req.file});
+});
 
 // route setup
 app.use('/', homeRouter);
--- controllers/home.js
+++ controllers/home.js
@@ -58,5 +58,5 @@ const HomeController = {
   },
 };
 
-module.exports = HomeController;
 
+module.exports = HomeController;
