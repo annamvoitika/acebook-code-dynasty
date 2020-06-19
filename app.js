fe467da9ee946d40bbe120f31f5c964ecdac7f18
kate smith <kateesmith4994@gmail.com>
2020-06-19 09:11:59 +0100

    Photos now show on screen

--- app.js
+++ app.js
@@ -91,6 +91,30 @@ app.post('/upload', upload.single('file'),(req, res) => {
   res.redirect('/photos');
 });
 
+// @route GET /image/:filename
+// @desc Display Image
+app.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
+
 app.delete('/files/:id', (req, res) => {
   gfs.remove({_id: req.params.id, root: 'uploads'}, (err, gridStore) => {
     if(err) {
